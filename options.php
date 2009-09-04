fbbadd9466879a923d9464b9824ebaa146bb7d52
julianwp <julianwp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-04 23:32:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/easy-meta/trunk@152122 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ options.php
@@ -0,0 +1,61 @@
+<?php
+if ($_POST["action"]=="add") {
+	$option = get_option("meta");
+	$option .= $_POST["name"]."===".$_POST["value"].";;;";
+	update_option("meta",$option);
+	echo '<div class="updated fade">New Meta Tag Registered !</div>';
+}
+if ($_POST["action"]=="remove") {
+	$option = explode(";;;",get_option("meta"));
+	$new = array();
+	for ($i=0;$i<count($option);$i++) {
+		$meta = explode("===",$option[$i]);
+		if ($meta[0]<>$_POST["delete"]) array_push($new,$option[$i]);
+	}
+	$option = join(";;;",$new);
+	update_option("meta",$option);
+	echo '<div class="updated fade">Meta Tag Removed !</div>';
+}
+?>
+<div class="wrap">
+<?php if(function_exists('screen_icon')) screen_icon(); ?>
+<h2>Easy Meta</h2>
+<em>You can easily add, remove, or edit any of your meta tag information here. For example for claiming your blog by put some meta tag information.</em>
+<style>
+tr {background-color:#FFF;}
+td, th {padding:5px;}
+th {background-color:#FFC;}
+</style>
+<table>
+<tr>
+<th>Meta Name</th>
+<th>Meta Value</th>
+<th>Action</th>
+</tr>
+<?php
+$data = get_option("meta");
+$metas = explode(";;;",$data);
+for ($i=0;$i<count($metas)-1;$i++) {
+	$meta = explode("===",$metas[$i]);
+	echo "<form method='POST'>";
+	echo "<input type='hidden' name='delete' value='".$meta[0]."' />";
+	echo "<input type='hidden' name='action' value='remove' />";
+	echo "<tr>";
+	echo "<td>".$meta[0]."</td>";
+	echo "<td>".$meta[1]."</td>";
+	echo "<td><input class='button' type='submit' value='Delete'/></td>";
+	echo "</tr>";
+	echo "</form>";
+}
+?>
+</table>
+
+<h2>Add New Meta Tag Record</h2>
+<form method="post">
+<input type="hidden" name="action" value="add" />
+<p>Meta Tag Name  :<input name="name" /></p>
+<p>Meta Tag Value :<input name="value" /></p>
+<p><input type="submit" class="button-primary" value="Add New" /></p>
+</form>
+
+</div>
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Plugin Name ===
+Contributors: Julian Widya Perdana
+Donate link: http://mr.hokya.com/donate
+Tags: meta, tag, html, admin, head
+Requires at least: 2.0.2
+Tested up to: 2.8.2
+Stable tag: 4.3
+
+Help you input meta tag information for claiming reason or other purposes.
+
+== Description ==
+
+Help you input meta tag information for claiming reason or other purposes.
+
+You just have to input the meta asked on the Option > Easy Meta tab and the plugin will automatically generate meta tag information on every page of your WordPress links. It will always show even you change your theme as many as you want and as long as the plugin is active. have a little fun with it ! :-)
+
+Already fixed the bug, thanks to Dan :-)
+
+== Installation ==
+
+
+1. Upload the zipped file to yoursite/wp-content/plugins
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Open up EasyIcon configuration tab in Option tab.
